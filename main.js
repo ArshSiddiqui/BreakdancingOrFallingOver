8d9a3b18d49038b2b1fb6917ae6a26e10ed1ec8a
Arsh Siddiqui <ArshSiddiqui@users.noreply.github.com>
2021-08-17 12:49:59 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,16 @@ var randNum = Math.floor(Math.random() * randPix.length);
 document.getElementById("image").src = randPix[randNum];
 document.write(randPix[randNum]);
 function breakd(){
-  document.getElementById("btnbd").innerHTML = "CLICKED!";
+  if (randPix[randNum].charAt(0) === 'b'){
+    document.getElementById("btnbd").innerHTML = "CORRECT!";
+  } else {
+    document.getElementById("btnbd").innerHTML = "WRONG!";
+  }
 }
 function fallo(){
-  document.getElementById("btnfo").innerHTML = "WOOW!";
+  if (randPix[randNum].charAt(0) === 'f'){
+    document.getElementById("btnfo").innerHTML = "CORRECT!";
+  } else{
+    document.getElementById("btnfo").innerHTML = "WRONG!";
+  }
 }
