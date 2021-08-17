5439b65c02cc6c3485a354d0519cecbb4f8d4730
Arsh Siddiqui <ArshSiddiqui@users.noreply.github.com>
2021-08-17 12:42:11 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ var randPix = new Array("bd1.jpg", "fo1.jpg");
 var randNum = Math.floor(Math.random() * randPix.length);
 document.getElementById("image").src = randPix[randNum];
 document.write(randPix[randNum]);
+function breakd(){
+  document.getElementById("btnbd").innerHTML = "CLICKED!";
+}
+function fallo(){
+  document.getEllementById("btnfo").innerHTML = "WOOW!";
+}
