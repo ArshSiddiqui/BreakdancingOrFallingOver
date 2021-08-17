50dae7391186ad667f24c921380b4d962c63bd10
Arsh Siddiqui <ArshSiddiqui@users.noreply.github.com>
2021-08-17 15:36:19 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,23 @@
 console.log('Hello World');
-var randPix = new Array("bd1.jpg", "fo1.jpg", "bd2.jpg", "fo2.jpg", "bd3.jpg", "fo3.jpg", "bd4.jpg", "fo4.jpg", "bd5.jpg", "fo5.jpg", "bd6.jpg", "fo6.jpg");
+var randPix = new Array("bd1.jpg", "fo1.jpg", "bd2.jpg", "fo2.jpg", "bd3.jpg", "fo3.jpg", "bd4.jpg", "fo4.jpg", "bd5.jpg", "fo5.jpg", "bd6.jpg", "fo6.jpg", "bd7.jpg", "fo7.jpg", "bd8.jpg", "fo8.jpg", "bd9.jpg", "fo9.jpg", "bd10.jpg", "fo10.jpg");
 var randNum = Math.floor(Math.random() * randPix.length);
 document.getElementById("image").src = randPix[randNum];
 document.write(randPix[randNum]);
 function breakd(){
   if (randPix[randNum].charAt(0) === 'b'){
     document.getElementById("btnbd").innerHTML = "CORRECT!";
-    window.setTimeout(1000*60*60);
     window.setTimeout(window.location.reload(), 1000*60*60);
   } else {
     document.getElementById("btnbd").innerHTML = "WRONG!";
-    window.setTimeout(1000*60*60);
     window.setTimeout(window.location.reload(), 1000*60*60);
   }
 }
 function fallo(){
   if (randPix[randNum].charAt(0) === 'f'){
     document.getElementById("btnfo").innerHTML = "CORRECT!";
-    window.setTimeout(1000*60*60);
     window.setTimeout(window.location.reload(), 1000*60*60);
   } else {
     document.getElementById("btnfo").innerHTML = "WRONG!";
-    window.setTimeout(1000*60*60);
     window.setTimeout(window.location.reload(), 1000*60*60);
   }
 }
