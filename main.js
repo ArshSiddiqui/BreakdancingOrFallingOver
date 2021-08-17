450334bb8792072a94174f21bed1f4d4f0071120
Arsh Siddiqui <ArshSiddiqui@users.noreply.github.com>
2021-08-17 12:14:19 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
 console.log('Hello World');
+var randPix = new Array("bd1.jpg", "fo1.jpg");
+var randNum = Math.floor(Math.random() * randPix.length);
+document.getElementById("image").src = randPix[randNum];
